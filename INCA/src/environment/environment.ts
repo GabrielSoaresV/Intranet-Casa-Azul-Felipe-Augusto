36a3644001f0a9987b6c8561ecfa7231d740da66
Gabriel Soares <122445286+GabrielSoaresV@users.noreply.github.com>
2025-12-03 15:56:24 -0300

    Atualizar o environment.ts

--- INCA/src/environment/environment.ts
+++ INCA/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 
-  apiDemanda: 'http:192.168.1.47//:8080',
+  apiDemanda: 'http://192.168.1.47:8080',
   apiGestao:  'http://192.168.1.47:8082'
 };
